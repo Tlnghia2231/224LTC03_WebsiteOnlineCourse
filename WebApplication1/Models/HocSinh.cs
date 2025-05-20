79b6f04119fd6bd8a2fb742b2a50aa020b60ab78
TranLamNghia <tranlam1142004@gmail.com>
2025-05-21 00:35:37 +0700

    Khoi chay WebApplication1 + Insert model tu csdl

---
+++ WebApplication1/Models/HocSinh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class HocSinh
+{
+    public string MaHocSinh { get; set; } = null!;
+
+    public string HoTen { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? DienThoai { get; set; }
+
+    public DateOnly? NgaySinh { get; set; }
+
+    public string? GioiTinh { get; set; }
+
+    public string? DiaChi { get; set; }
+
+    public DateTime NgayDangKy { get; set; }
+
+    public virtual ICollection<KhoaHocHocSinh> KhoaHocHocSinhs { get; set; } = new List<KhoaHocHocSinh>();
+}
---
+++ WebApplication1/Models/KhoaHocHocSinh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class KhoaHocHocSinh
+{
+    public string MaKhoaHoc { get; set; } = null!;
+
+    public string MaHocSinh { get; set; } = null!;
+
+    public DateTime NgayDangKy { get; set; }
+
+    public DateTime NgayHetHan { get; set; }
+
+    public virtual HocSinh MaHocSinhNavigation { get; set; } = null!;
+
+    public virtual KhoaHoc MaKhoaHocNavigation { get; set; } = null!;
+}
