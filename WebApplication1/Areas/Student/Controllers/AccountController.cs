de0151c78e5dab3ad599f1d2f983ea9a0af7adec
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-05-26 10:54:04 +0700

    Luu tam thoi luc login thanh cong + vao duoc my-learning

---
+++ WebApplication1/Areas/Student/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Student.Controllers
+{
+    [Area("Student")]
+    [Authorize(Roles = "Student")]
+    public class AccountController : Controller
+    {
+
+        private readonly ILogger<AccountController> _logger;
+        private readonly AppDbContext _context;
+
+        public AccountController(ILogger<AccountController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/student/mylearning")]
+        public IActionResult MyLearning()
+        {
+            return View();
+        }
+    }
+}
